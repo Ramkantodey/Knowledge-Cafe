7743c0713aaa64dbf1d3d9c280c5b8f27e0c752b
Ramkantodey <ramkantodey@gmail.com>
2024-10-22 18:19:02 +0600

    sites is done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,15 +1,36 @@
 import PropTypes from 'prop-types';
-const Blog = ({ blog }) => {
-    console.log(blog);
+const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
+    const { id, title, cover, reading_time, author, author_img, posted_date, hashtags } = blog;
     return (
-        <div>
-
+        <div className='mb-20'>
+            <img className='rounded-md w-full mb-4' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4 items-center'>
+                <div className='flex gap-6'>
+                    <img className='w-12 h-12 rounded-full' src={author_img} alt="" />
+                    <div>
+                        <h3 className='text-2xl font-bold'>{author}</h3>
+                        <p className='text-gray-500'>{posted_date}</p>
+                    </div>
+                </div>
+                <div className='flex items-center gap-4'>
+                    <span>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmark(blog)}><img className='w-[24px] ' src="https://i.imgur.com/ZCd26N8.png" alt="" /></button>
+                </div>
+            </div>
+            <h2 className='text-4xl font-semibold text-[#111111] my-4'>{title}</h2>
+            <p className='text-yellow-800'>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href=""> {hash} </a></span>)
+                }
+            </p>
+            <button className='mt-4 text-purple-600 font-bold underline' onClick={() => handleMarkAsRead(id, reading_time)}>Mark as read</button>
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func
 }
 
 export default Blog;
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,24 @@
+import PropTypes from "prop-types";
+import Bookmark from "../Bookmark/Bookmark";
 
 
-const Bookmarks = () => {
+const Bookmarks = ({ bookmarks, readingTime }) => {
     return (
-        <div className="md:w-1/3">
-            <h2>Bookmarks</h2>
+        <div className="md:w-1/3 bg-gray-100 ml-4 mt-2 pt-4 rounded-md text-center">
+            <div className="p-4 border-2 rounded-md bg-purple-100 border-purple-600 ">
+                <h3 className="text-2xl font-bold text-purple-800">Spent time on read : {readingTime} min</h3>
+            </div>
+            <h2 className="text-3xl text-center  text-blue-400">Bookmarked Blogs:    {bookmarks.length}</h2>
+            {
+                bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>)
+            }
         </div>
     );
 };
 
-export default Bookmarks;
+Bookmarks.propTypes = {
+    bookmarks: PropTypes.array,
+    readingTime: PropTypes.number
+}
+
+export default Bookmarks
