1ae1adeb3a83cc049ed4012a2095c6ab6f5d4cf1
Ramkantodey <ramkantodey@gmail.com>
2024-10-22 14:41:15 +0600

    kico na

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -13,6 +13,9 @@ const Blogs = () => {
     return (
         <div className="w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+
+            }
         </div>
     );
 };
